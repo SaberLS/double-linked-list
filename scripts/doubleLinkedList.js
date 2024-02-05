e33ea533cfd6695e45e97be0186e7faa91b7cc00
SaberLS <patryk.platek19@gmail.com>
2024-02-05 21:25:30 +0100

    initial commit

---
+++ scripts/doubleLinkedList.js
@@ -0,0 +1,13 @@
+import Node from "./Node.js";
+
+export default class doubleLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    log() {
+
+    }
+}
---
+++ scripts/node.js
@@ -0,0 +1,11 @@
+export default class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+        this.previous = null;
+    }
+
+    log() {
+        return `{\n\tdata: ${this.data},\n\tnext:${(this.next ? this.next.data : null)},\n\t${(this.previous ? this.previous.data : null)}\n}`;
+    }
+}

8f1e73ac789af0209f3d8c6d86f02d777594c4b6
SaberLS <patryk.platek19@gmail.com>
2024-02-10 18:58:04 +0100

    created test for bubbleSort

---
+++ __tests__/bubbleSort/BubbleSort.test.js
@@ -0,0 +1,22 @@
+import doubleLinkedList from "../../scripts/doubleLinkedList.js";
+import BubbleSort from "../../scripts/BubbleSort.js";
+import Node from "../../scripts/Node.js";
+
+describe("BubbleSort", () => {
+    const testList = new doubleLinkedList();
+
+    test("it returns doubleLinkedList sorted in ascending order by data", () => {
+        testList.push(new Node(3));
+        testList.push(new Node(2));
+        testList.push(new Node(1));
+
+        const expectedResult = new doubleLinkedList;
+
+        expectedResult.push(new Node(1));
+        expectedResult.push(new Node(2));
+        expectedResult.push(new Node(3));
+
+        BubbleSort(testList);
+        expect(expectedResult).toEqual(expectedResult);
+    });
+});
